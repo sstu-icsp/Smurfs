6803edc10a1e3b914c6e40d706120725ac986ab3
BAS1404 <bas140496@mail.ru>
2017-11-04 15:38:06 +0400

    init

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // server.js
-const express        = require('express');
-const bodyParser     = require('body-parser');
-const app            = express();
-const port = 8000;
-require('./routes')(app, {});
+var express = require("express");
+var app = express();
+var bodyParser     = require('body-parser');
+var port = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+app.use(express.static('files'));
+app.use(bodyParser.urlencoded({ extended: true }));
+require('./routes/all_routes')(app);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 app.listen(port, () => {
   console.log('We are live on ' + port);
 });
